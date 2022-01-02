d032b543875411befe5c3b396aab441ac9f8a395
patrykosak <pionchaoo@gmail.com>
2022-01-02 23:40:47 +0100

    displaying images

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,8 +10,14 @@ export const Results = () => {
   const location = useLocation();
 
   useEffect(() => {
-    getResults("/search/q=Java&num=40");
-  }, []);
+    if (searchTerm) {
+      if (location.pathname === "/videos") {
+        getResults(`/search/q=${searchTerm} videos`);
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />;
 
@@ -26,19 +32,36 @@ export const Results = () => {
                   {link.length > 30 ? link.substring(0, 30) : link}
                 </p>
                 <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
-                    {title}
+                  {title}
                 </p>
               </a>
             </div>
           ))}
         </div>
       );
+    case "/images":
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {results?.image_results?.map(
+            ({ image, link: { href, title } }, index) => (
+              <a
+                className="sm:p-3 p-5"
+                href={href}
+                key={index}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={image?.src} alt={title} loading="lazy" />
+                <p className="w-36 break-words text-sm mt-2">{title}</p>
+              </a>
+            )
+          )}
+        </div>
+      );
     case "/news":
       return "NEWS";
     case "/videos":
       return "VIDEOS";
-    case "/images":
-      return "IMAGES";
 
     default:
       return "ERROR!";
