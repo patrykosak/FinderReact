f783bccb62e8efa183a392935a08a08fd448fd29
patrykosak <pionchaoo@gmail.com>
2022-01-03 18:30:13 +0100

    small fixes

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const links = [
     { url: '/search', text: '🔎 All' },
@@ -11,10 +11,10 @@ const links = [
 export const Links = () => {
     return (
         <div className='flex sm:justify-around justify-between items-center mt-4'>
-            {links.map(({url, text})=>(
-                <NavLink to={url} className="m-2 mb-0" activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">
+            {links.map(({url, text},index)=>(
+                <NavLink  key={index} to={url} className="m-2 mb-0" activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">
                     {text}
-                </NavLink>
+                </NavLink >
             ))}
         </div>
     )
