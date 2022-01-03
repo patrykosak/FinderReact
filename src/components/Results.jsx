6cd6eb6ade2be54c1e3eba0740ec225e421e7fe3
patrykosak <pionchaoo@gmail.com>
2022-01-03 15:52:59 +0100

    displaying newses

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -25,7 +25,7 @@ export const Results = () => {
     case "/search":
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-          {results?.results?.map(({ link, title }, index) => (
+          {results?.map(({ link, title }, index) => (
             <div key={index} className="md:w-2/5 w-full">
               <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-sm">
@@ -42,7 +42,7 @@ export const Results = () => {
     case "/images":
       return (
         <div className="flex flex-wrap justify-center items-center">
-          {results?.image_results?.map(
+          {results?.map(
             ({ image, link: { href, title } }, index) => (
               <a
                 className="sm:p-3 p-5"
@@ -59,7 +59,24 @@ export const Results = () => {
         </div>
       );
     case "/news":
-      return "NEWS";
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
+          {results?.map(({ links, id, source, title }) => (
+            <div key={id} className="md:w-2/5 w-full">
+              <a href={links?.[0].href} target="_blank" rel="noreferrer" className="hover:underline">
+                <p className="text-lg dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+                <div className="flex gap-4">
+                  <a href={source?.href} target="_blank" rel="noreferrer">
+                    {source?.href}
+                  </a>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
     case "/videos":
       return "VIDEOS";
 
